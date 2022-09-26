16ae1b42677986ed4bd52559ac38f54fd76826fa
Farrel Mohammad <farrel.adel@gmail.com>
2022-09-26 15:32:26 +0700

    minor update

--- index.php
+++ index.php
@@ -9,53 +9,46 @@
 </head>
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET">
+        <div>
+            <label for="string_input">Masukkan String: </label>
+            <input type="text" name="string_input">
+        </div>
+        <div>
+            <label for="loop_input">Masukkan Pengulangan: </label>
+            <input type="number" name="loop_input" value="0">
+        </div>
+        <div class="btn-con">
+            <input class="btn" type="submit" value="Submit" name="submit">
+            <input class="btn" type="reset" value="Reset">
+            <a href="index.php" class="btn">Clear</a>
+        </div>
+    </form>
+
     <div>
-        <label for="string_input">Masukkan String: </label>
-        <input type="text" name="string_input">
-    </div>
-    <div>
-        <label for="loop_input">Masukkan Pengulangan: </label>
-        <input type="number" name="loop_input" value="0">
-    </div>
-    <div class="btn-con">
-        <input class="btn" type="submit" value="Submit" name="submit">
-        <input class="btn" type="reset" value="Reset">
-        <a href="index.php" class="btn">Clear</a>
-    </div>
-</form>
-
-<div>
-    <?php
-        $input_arr = [];
-        
-        if(isset($_GET['submit'])) {
-            $string_input = filter_input(INPUT_GET, 'string_input', FILTER_SANITIZE_SPECIAL_CHARS) ;
-            $loop_input = $_GET['loop_input'];
-
+        <?php
+            $input_arr = [];
             
-            
-            if(strlen($string_input) > 0 && $loop_input > 0){
-                $i = 0;
+            if(isset($_GET['submit'])) {
+                $string_input = filter_input(INPUT_GET, 'string_input', FILTER_SANITIZE_SPECIAL_CHARS) ;
+                $loop_input = $_GET['loop_input'];            
+                if(strlen($string_input) > 0 && $loop_input > 0){
+                    $i = 0;
 
-                while($i < $loop_input) {
-                    $i++;
-                    echo "
-                    <div class='output'>    
-                    {$string_input} {$i}
-                    </div>
-                    ";
+                    while($i < $loop_input) {
+                        $i++;
+                        echo "
+                        <div class='output'>    
+                        {$string_input} {$i}
+                        </div>
+                        ";
+                    }
+                    
+                    echo $loop_input % 2 == 0 ? 
+                    "<div class='output last'>  $loop_input merupakan Bilangan Genap</div>"
+                    : "<div class='output last'> $loop_input merupakan Bilangan Ganjil</div>";
                 }
-                
-
-                echo $loop_input % 2 == 0 ? "<div class='output last'>  $loop_input merupakan Bilangan Genap</div>" : "<div class='output last'> $loop_input merupakan Bilangan Ganjil</div>";
-            }
-        }
-
-        
-    ?>
-</div>
-
-
-
+            }        
+        ?>
+    </div>
 </body>
 </html>
